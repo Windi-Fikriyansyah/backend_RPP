73946f1bc2688ed3e5f52d2cabdde22e68bba8ef
windi fikriyansyah <diwin321@gmail.com>
2026-01-17 00:57:29 +0700

    s

--- app/security.py
+++ app/security.py
@@ -1,6 +1,6 @@
 from passlib.context import CryptContext
 
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+pwd_context = CryptContext(schemes=["bcrypt_sha256", "bcrypt"], deprecated="auto")
 
 def verify_password(plain_password, hashed_password):
     return pwd_context.verify(plain_password, hashed_password)
